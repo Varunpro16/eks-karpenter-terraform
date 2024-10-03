522ca567bb9b6595100e15e36ab3e0e5efa83960
Varunpro16 <91467967+Varunpro16@users.noreply.github.com>
2024-10-03 18:19:01 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+
+terraform {
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+  }
+}
+
+
+
