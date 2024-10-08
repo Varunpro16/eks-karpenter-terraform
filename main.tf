16435a996b6b93010ec0588a4b84ff7cb9c66e1d
Varunpro16 <91467967+Varunpro16@users.noreply.github.com>
2024-10-08 13:57:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,5 +17,20 @@ terraform {
   }
 }
 
+data "aws_eks_cluster" "example" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.example.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.example.name]
+  }
+}
+
 
 
