8c265aac666b604b935096702e032ff5419ebccc
Varunpro16 <91467967+Varunpro16@users.noreply.github.com>
2024-10-08 13:58:29 +0530

    Create per_vol.tf

---
+++ per_vol.tf
@@ -0,0 +1,26 @@
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"  # Replace with your AZ
+  size              = 10  # 10 GiB
+  type              = "gp3"  # EBS volume type
+}
+resource "kubernetes_persistent_volume" "example_pv" {
+  metadata {
+    name = "example-pv"
+  }
+
+  spec {
+    capacity = {
+      storage = "10Gi"  # Capacity of the PV
+    }
+    access_modes = ["ReadWriteOnce"]
+
+    persistent_volume_source {
+      aws_elastic_block_store {
+        volume_id = aws_ebs_volume.example.id  # Bind EBS volume ID
+        fs_type   = "ext4"  # Filesystem type
+      }
+    }
+
+    persistent_volume_reclaim_policy = "Retain"  # Reclaim policy
+  }
+}
